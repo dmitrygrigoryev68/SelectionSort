47fce0676efbd6ac6159bbeeb5523f08fd11ec99
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-09-07 17:31:27 +0200

    last commit

--- src/SelectSort.java
+++ src/SelectSort.java
@@ -6,38 +6,46 @@ public class SelectSort {
 
         int[] arr = new int[10];
         fillArr(arr);
-        printArr(arr);
+        print(arr);
         sortArr(arr);
-        System.out.println("\nAfter sorting \n");
-        printArr(arr);
+        print();
+        print(arr);
     }
-        public static void sortArr ( int arr[]){
-            int j, tempElement = 0;
-            for (int i=0;i < arr.length;i++){
-                j = searchIndexOfMin(arr,i);
-                tempElement = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tempElement;
+
+    static void sortArr(int arr[]) {
+        int indexOfMin, tempElement = 0;
+        for (int i = 0; i < arr.length; i++) {
+            indexOfMin = searchIndexOfMin(arr, i);
+            tempElement = arr[i];
+            arr[i] = arr[indexOfMin];
+            arr[indexOfMin] = tempElement;
+        }
+    }
+
+    static int searchIndexOfMin(int arr[], int start) {
+        int indexOfMin = start;
+        for (int i = start + 1; i < arr.length; i++) {
+            if (arr[i] < arr[indexOfMin]) {
+                indexOfMin = i;
             }
         }
-        public static int searchIndexOfMin(int arr[], int start){
-            int indexOfMin = start;
-            for (int i = start+1; i <arr.length; i++) {
-                if (arr[i] < arr[indexOfMin]) {
-                    indexOfMin = i;
-                }
-            }
-            return indexOfMin;
+        return indexOfMin;
+    }
+
+    static void fillArr(int arr[]) {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
         }
-        public static void fillArr(int arr[]){
-             Random random = new Random();
-             for (int i = 0; i < arr.length ; i++) {
-                 arr[i] = random.nextInt(100);
-             }
+    }
+
+    static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
-        public static void printArr ( int arr[]){
-             for (int i = 0; i < arr.length; i++) {
-                 System.out.println(arr[i]);
-             }
-        }
+    }
+
+    static void print() {
+        System.out.println("\nAfter sorting \n");
+    }
 }
